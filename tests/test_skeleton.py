82d4c15990fd68362a2e48a970f7199a03d1f6c7
Kees den Heijer <kees.denheijer@deltares.nl>
2018-09-07 11:14:52 +0200

    example test added

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -2,16 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from terminal_optimization.skeleton import fib
+import unittest
+from terminal_optimization import core
 
 __author__ = "VANOORD\MRV"
 __copyright__ = "VANOORD\MRV"
 __license__ = "gpl3"
 
 
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
+class Test_vessel_properties_mixin(unittest.TestCase):
+
+    def test_init(self):
+        # define sample input
+        vessel_type = 'blabla'
+        call_size = None
+        LOA = None
+        draft = None
+        beam = None
+        max_cranes = None
+        all_turn_time = None
+        mooring_time = None
+        demurrage = None
+
+        # initialize object
+        VP = core.vessel_properties_mixin(vessel_type, call_size, LOA, draft, beam, max_cranes, all_turn_time, mooring_time, demurrage)
+
+        # make sure that argument is passed to the envisaged attribute
+        assert VP.vessel_type == vessel_type
+        assert VP.call_size == call_size
+        # ... do this for all arguments
