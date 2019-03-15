24d6a3f45ccbd41a92c4277a4cd9f8273e6637f6
VANOORD\MRV <mark.vankoningsveld@vanoord.com>
2019-03-15 19:40:58 +0100

    Changed __init__

--- src/terminal_optimization/__init__.py
+++ src/terminal_optimization/__init__.py
@@ -6,6 +6,5 @@ __author__ = """Mark van Koningsveld"""
 __email__ = 'm.vankoningsveld@tudelft.nl'
 __version__ = '0.1.0'
 
-from .financial_analysis import *
-from .forecast import *
-from .infrastructure import *
+from .objects import *
+from .system import *
